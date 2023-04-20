70ff65b6603f376dd06c490ac2020718a1681966
Ilia Grigorev <subarist007@mail.ru>
2023-04-20 22:46:10 +0200

    start users authentication 4

--- users/views.py
+++ users/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+
+from users.models import User
+from users.forms import UserLoginForm, UserRegistrationForm
 
 def login(request):
-    context = {
-        'title': 'Вход',
-    }
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
     return render(request, 'users/login.html', context)
 
 def registration(request):
-    context = {
-        'title': 'Регистрация',
-    }
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('login'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
     return render(request, 'users/registration.html', context)
