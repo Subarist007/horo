d7e344a5c87a5cb7d1933f9b9056a79e55f35e66
Ilia Grigorev <subarist007@mail.ru>
2023-04-21 19:08:28 +0200

    start users authentication 5

--- users/forms.py
+++ users/forms.py
@@ -11,12 +11,15 @@ class UserLoginForm(AuthenticationForm):
         model = User
         fields = ('username', 'password')
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Имя'}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Фамилия'}))
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Имя пользователя'}))
     email = forms.CharField(widget=forms.EmailInput(attrs={'placeholder': 'Электронная почта'}))
-    date_birth = forms.DateField(widget=forms.DateInput(attrs={'placeholder': 'Дата рождения гггг-мм-дд'}))
+    date_birth = forms.DateField(widget=DateInput(attrs={'placeholder': 'Дата рождения'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Подтвердите пароль'}))
     class Meta:
