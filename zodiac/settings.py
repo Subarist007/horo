2dbf361b29f72e487d5ad41108dbb344f4422fb1
Ilia Grigorev <ilia_grigorev@mail.ru>
2023-05-04 19:34:26 +0200

    edits

--- zodiac/settings.py
+++ zodiac/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'zodiac.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'horoscope',
-        'USER': 'postgres',
-        'PASSWORD': 'Stleitenant7789',
+        'NAME': os.getenv('name'),
+        'USER': os.getenv('user'),
+        'PASSWORD': os.getenv('password'),
         'HOST': 'localhost',
     }
 }
